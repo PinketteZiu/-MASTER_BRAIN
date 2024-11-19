63de61e9440a88783ce7bd9a03ea69dc01cb733e
Vincent Lelong <vincent.lelong1995@gmail.com>
2024-11-19 17:06:58 +0100

    ajout d'une liste de compétence

--- app/models/brain.rb
+++ app/models/brain.rb
@@ -2,6 +2,10 @@ class Brain < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
+  VALID_COMPETENCES = %w[Analyse Créativité Logique Mémoire Communication Apprentissage Gestion_émotionnelle]
+
+  validates :competence, inclusion: { in: VALID_COMPETENCES,
+    message: "%{value} n'est pas une compétence valide" }
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :address, presence: true
