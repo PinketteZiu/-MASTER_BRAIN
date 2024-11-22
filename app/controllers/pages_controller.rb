c1027d6157764bc2fa166d4faaafce83ef264101
Antoine Gach <antoine.gach@orange.fr>
2024-11-22 14:30:19 +0100

    c good, 4 cerveaux page accueil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @brains = Brain.all
+    @brains = Brain.all.last(4)
   end
 
   def my_account
